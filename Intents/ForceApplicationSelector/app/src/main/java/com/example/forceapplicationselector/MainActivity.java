1c1dcddc618d2712effddbce60da8b3aed8e8bf5
fox <a88667733zx@163.com>
2023-08-25 13:33:06 +0800

    Create an ForceApplicationSelector in intents and updata README.md in intents

---
+++ Intents/ForceApplicationSelector/app/src/main/java/com/example/forceapplicationselector/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.forceapplicationselector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+    private void init(){
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendIntent = new Intent(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "text");
+                sendIntent.setType("text/plain");
+                // 获取资源的字符串
+                String title = getResources().getString(R.string.app_name);
+
+                // 创建意图并设置对话框名称
+                Intent chooser = Intent.createChooser(sendIntent, title);
+                try {
+                    startActivity(chooser);
+                }catch (Exception e){
+                    Toast.makeText(MainActivity.this, ""+e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
